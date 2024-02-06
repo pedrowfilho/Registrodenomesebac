233c6262ef51ca3902910b51ce8784cd79e2194d
Pedro Wagner <44990312+pedrowfilho@users.noreply.github.com>
2024-02-06 16:50:40 -0300

    Versão final
    Versão final do Sistema de Registro de Nomes EBAC.

--- sistema_registro_nomes.c
+++ sistema_registro_nomes.c
@@ -3,8 +3,8 @@
 #include <locale.h> //biblioteca de alocação de texto por região
 #include <string.h> //biblioteca responsável pelas strings
 
-int incluir(){
-	printf("\tINCLUIR REGISTRO\n");
+int incluir(){ //função responsável por cadastrar nomes no sistema
+	printf("\tINCLUIR REGISTRO\n\n");
 	
 	//declaração das variáveis
 	char arquivo[40];
@@ -21,7 +21,7 @@ int incluir(){
 	FILE *file; //chama a função FILE do sistema para trabalhar com arquivos
 	
 	file = fopen(arquivo, "w"); //cria o arquivo
-	fprintf(file, cpf); //salva no arquivo o valor da variável
+	fprintf(file, cpf); //escreve no arquivo o valor da variável cpf
 	fclose(file); //fecha o arquivo
 	
 	file = fopen(arquivo, "a"); //"a" atualiza o arquivo
@@ -32,7 +32,7 @@ int incluir(){
 	scanf("%s", nome);
 	
 	file = fopen(arquivo, "a"); //"a" atualiza o arquivo
-	fprintf(file, nome); //salva o valor da variável nome no arquivo
+	fprintf(file, nome); //escreve o valor da variável nome no arquivo
 	fclose(file); //fecha o arquivo
 	
 	file = fopen(arquivo, "a"); //"a" atualiza o arquivo
@@ -54,14 +54,15 @@ int incluir(){
 	scanf("%s", cargo);
 	
 	file = fopen(arquivo, "a"); //"a" atualiza o arquivo
-	fprintf(file, cargo); //salva o valor da variável sobrenome no arquivo
+	fprintf(file, cargo); //escreve o valor da variável sobrenome no arquivo
 	fclose(file); //fecha o arquivo
 	
 	system("pause");
+	
 }
 
 int consultar(){
-	printf("\tCONSULTAR REGISTRO\n");
+	printf("\tCONSULTAR REGISTRO\n\n");
 	setlocale(LC_ALL, "Portuguese"); //seleciona a linguagem
 	
 	char cpf[40];
@@ -88,7 +89,7 @@ int consultar(){
 }
 
 int excluir(){
-	printf("\tEXCLUIR REGISTRO\n");
+	printf("\tEXCLUIR REGISTRO\n\n");
 	char cpf[40];
 	
 	printf("Informe o CPF para excluir: ");
@@ -115,44 +116,125 @@ char menu(){
 	printf("\t1 - Incluir\n");
 	printf("\t2 - Consultar\n");
 	printf("\t3 - Excluir\n");
-	printf("\t4 - Sair\n\n");
+	printf("\t4 - Cadastrar usuário\n");
+	printf("\t5 - Sair\n\n");
+}
+
+int cadastrarUsuario(){
+	printf("\tCADASTRAR USUÁRIO\n\n");
+	
+	//declaração das variáveis
+	char arquivo[40];
+	char email[40];
+	char nome[40];
+	char sobrenome[40];
+	char senha[10];
+	
+	printf("email: ");
+	scanf("%s", email);
+	
+	strcpy(arquivo, email); //copia o valor da string
+	
+	FILE *file; //chama a função FILE do sistema para trabalhar com arquivos
+	
+	file = fopen(arquivo, "w"); //cria o arquivo
+	fprintf(file, email); //salva no arquivo o valor da variável
+	fclose(file); //fecha o arquivo
+	
+	file = fopen(arquivo, "a"); //"a" atualiza o arquivo
+	fprintf(file, ","); //inclui uma , após o valor da última variável armazenada
+	fclose(file); //fecha o arquivo
+	
+	printf("senha: ");
+	scanf("%s", senha);
+	
+	file = fopen(arquivo, "a"); //"a" atualiza o arquivo
+	fprintf(file, senha); //salva o valor da variável nome no arquivo
+	fclose(file); //fecha o arquivo
+	
+	file = fopen(arquivo, "a"); //"a" atualiza o arquivo
+	fprintf(file, ","); //inclui uma , após o valor da última variável armazenada
+	fclose(file); //fecha o arquivo
+	
+	printf("nome: ");
+	scanf("%s", nome);
+	
+	file = fopen(arquivo, "a"); //"a" atualiza o arquivo
+	fprintf(file, nome); //salva o valor da variável nome no arquivo
+	fclose(file); //fecha o arquivo
+	
+	file = fopen(arquivo, "a"); //"a" atualiza o arquivo
+	fprintf(file, ","); //inclui uma , após o valor da última variável armazenada
+	fclose(file); //fecha o arquivo
+	
+	printf("sobrenome: ");
+	scanf("%s", sobrenome);
+	
+	file = fopen(arquivo, "a"); //"a" atualiza o arquivo
+	fprintf(file, sobrenome); //salva o valor da variável nome no arquivo
+	fclose(file); //fecha o arquivo
+	
+	file = fopen(arquivo, "a"); //"a" atualiza o arquivo
+	fprintf(file, ","); //inclui uma , após o valor da última variável armazenada
+	fclose(file); //fecha o arquivo
+	
+	printf("Usuário cadastrado com sucesso!\n");
+	
+	system("pause");
 }
 
 int main(){
 	
 	int opcao = 0; //declara a varavel que vai armazenar a opcao escolhida pelo usuario
 	int laco = 1;
+	char senhadigitada[] = "a";
+	int verificacao;
 	
-	for(laco = 1; laco = 1;){
+	printf("*** SISTEMA DE REGISTRO DE NOMES ***\n");
+	printf("\t\tAcesse!\n\n");
+	printf("Digite sua senha: ");
+	scanf("%s", senhadigitada);
 	
-		setlocale(LC_ALL, "Portuguese"); //seleciona a linguagem
+	//compara se a senha digitada pelo usuário é a mesma da string
+	verificacao = strcmp(senhadigitada, "admin");
+	
+	if(verificacao == 0){
 		
-		menu();
+		for(laco = 1; laco = 1;){
 		
-		printf("Opção: ");
-		scanf("%d", &opcao); //lê a opção escolhida pelo usuário
-		
-		system("cls"); //limpa a tela
-		
-		switch(opcao){
-			case 1:
-				incluir();
-				break;
-			case 2:
-				consultar();
-				break;
-			case 3:
-				excluir();
-				break;
-			case 4:
-				printf("Obrigado por utulizar o sistema!");
-				return 0;
-				break;
-			default:
-				printf("\tOpção inválida!\n");
-				system("pause");
-				break;
+			setlocale(LC_ALL, "Portuguese"); //seleciona a linguagem
+			
+			menu();
+			
+			printf("Opção: ");
+			scanf("%d", &opcao); //lê a opção escolhida pelo usuário
+			
+			system("cls"); //limpa a tela
+			
+			switch(opcao){
+				case 1:
+					incluir();
+					break;
+				case 2:
+					consultar();
+					break;
+				case 3:
+					excluir();
+					break;
+				case 4:
+					cadastrarUsuario();
+					break;
+				case 5:
+					printf("Obrigado por utilizar o sistema!");
+					return 0; //sai do sistema
+					break;
+				default:
+					printf("\tOpção inválida!\n");
+					system("pause");
+					break;
+			}
 		}
-	}
+	}else
+		printf("Senha incorreta!");
 }
 
